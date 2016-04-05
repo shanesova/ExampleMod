1a9411a0b1052a221388ae05d3e0b6884fe1cf2b
Shane Sova <ssova@yahoo.com>
2016-04-05 04:28:49 -0600

    Fixed forgotten reference changes

--- src/main/java/com/example/examplemod/reference/Reference.java
+++ src/main/java/com/example/examplemod/reference/Reference.java
@@ -3,10 +3,10 @@ package com.example.examplemod.reference;
 public class Reference {
     public static final String MOD_ID = "examplemod";
     public static final String LOWERCASE_MOD_ID = MOD_ID.toLowerCase();
-    public static final String MOD_NAME = "Equivalent Exchange 3";
+    public static final String MOD_NAME = "Example Mod";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES = "required-after:Forge@[10.13.3,)";
+    public static final String DEPENDENCIES = "required-after:Forge@[12.16.0.1845,)";
     public static final String SERVER_PROXY_CLASS = "com.example.examplemod.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.example.examplemod.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.example.examplemod.client.gui.GuiFactory";
