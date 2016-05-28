7047877506ebfe68c9abcb7460d2831081b88794
Shane Sova <ssova@yahoo.com>
2016-05-27 19:48:22 -0600

    Add acceptable minecraft versions

--- src/main/java/com/example/examplemod/reference/Reference.java
+++ src/main/java/com/example/examplemod/reference/Reference.java
@@ -7,6 +7,7 @@ public class Reference {
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
     public static final String DEPENDENCIES = "required-after:Forge@[12.17.0.1909,)";
+    public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.9,1.10)";
     public static final String SERVER_PROXY_CLASS = "com.example.examplemod.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.example.examplemod.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.example.examplemod.client.gui.GuiFactory";
