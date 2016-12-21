33c131ffc9709fba47f5b41bc375e436c61d9b82
Shane Sova <ssova@yahoo.com>
2016-12-21 13:06:16 -0700

    Also accept 1.10.2

--- src/main/java/com/example/examplemod/reference/Reference.java
+++ src/main/java/com/example/examplemod/reference/Reference.java
@@ -6,8 +6,8 @@ public class Reference {
     public static final String MOD_NAME = "Example Mod";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES = "required-after:forge@[13.19.1.2189,)";
-    public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.11]";
+    public static final String DEPENDENCIES = "required-after:forge@[12.18.3.2185,)";
+    public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.10.2,1.11]";
     public static final String SERVER_PROXY_CLASS = "com.example.examplemod.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.example.examplemod.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.example.examplemod.client.gui.GuiFactory";
