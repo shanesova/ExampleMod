9affaf01da33cc6fff1b64240ef54216b05dd33d
Shane Sova <ssova@yahoo.com>
2017-02-27 11:43:17 -0700

    Update to minecraft 1.11.2 and latest forge, and auto generate json update file.

--- src/main/java/com/example/examplemod/reference/Reference.java
+++ src/main/java/com/example/examplemod/reference/Reference.java
@@ -6,10 +6,10 @@ public class Reference {
     public static final String MOD_NAME = "Example Mod";
     public static final String FINGERPRINT = "@FINGERPRINT@";
     public static final String MOD_VERSION = "@MOD_VERSION@";
-    public static final String DEPENDENCIES = "required-after:forge@[12.18.3.2185,)";
-    public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.10.2,1.11]";
+    public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2228,)";
+    public static final String ACCEPTEDMINECRAFTVERSIONS = "[1.11.2]";
     public static final String SERVER_PROXY_CLASS = "com.example.examplemod.proxy.ServerProxy";
     public static final String CLIENT_PROXY_CLASS = "com.example.examplemod.proxy.ClientProxy";
     public static final String GUI_FACTORY_CLASS = "com.example.examplemod.client.gui.GuiFactory";
-    public static final String UPDATE_URL = "https://raw.githubusercontent.com/shanesova/ExampleMod/master/update.json";
+    public static final String UPDATE_URL = "https://raw.githubusercontent.com/shanesova/ExampleMod/master/modid.json";
 }
